73851025d3fae4bddc247650f7f2e8c303fdf557
Yuni-Q <carnotcycle2@naver.com>
2020-01-15 16:00:10 +0900

    counter

--- src/counter.js
+++ src/counter.js
@@ -1,13 +1,26 @@
-export function createCounter() {
+export function createCounter(options = {}) {
+  let value = options.initVal || 0;
+  let min = options.min || -9999; // 최소값으로 설정해야 함
+  let max = options.max || 9999; // 최대값으로 설정해야 함
   return {
     val() {
-
+      return value;
     },
     inc() {
-
+      if (!this.isMax()) {
+        value++;
+      }
     },
     dec() {
-
+      if (!this.isMin()) {
+        value--;
+      }
+    },
+    isMax() {
+      return max === value;
+    },
+    isMin() {
+      return min === value;
     }
-  }
+  };
 }
